fa7a7d49123ee7ba0c9e17c06026417acefc1913
Melka Tolessa <melkatole1@gmail.com>
2023-02-21 00:31:20 +0300

    added major functionalities

--- scripts.js
+++ scripts.js
@@ -56,3 +56,38 @@ console.log(countries)
 function showMenu(){
     $(".regions").toggle()
 }
+
+function handleSearch(){
+    country = []
+    console.log($("#search").val())
+    fetch("./data.json")
+    .then(res=>res.json())
+    .then(data=>{
+        // data.length = 8;
+        let choice = $("#search").val()
+        console.log(typeof choice)
+        country = data.filter(con => {
+            // console.log(con.name, con["name"])
+            return con["name"].toLowerCase().match(choice)
+        })
+        countries = country.map(item => {
+            return (
+                `<div id=${item.numericCode} class="country--container">
+                    <img src=${item.flags.png} alt="">
+                    <div class="country--desc">
+                        <p class="country--name">${item.name}</p>
+                        <p class="population">Population: <span>${item.population}</span></p>
+                        <p class="region">Region: <span>${item.region}</span></p>
+                        <p class="capital">Capital: <span>${item.capital}</span></p>
+                        
+                    </div>
+                </div>`
+            )
+        })
+        console.log(countries)
+        $("#country--list").children().remove();
+        $("#country--list").append(countries)
+        $(".regions").toggle()
+    })
+    console.log(countries)
+}
