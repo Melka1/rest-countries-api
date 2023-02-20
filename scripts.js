8eef745bb8b86cf773011e566f61c24271b7d573
Melka Tolessa <melkatole1@gmail.com>
2023-02-20 22:30:53 +0300

    some functionality

---
+++ scripts.js
@@ -0,0 +1,58 @@
+let country = []
+let countries;
+fetch("./data.json")
+    .then(res=>res.json())
+    .then(data=>{
+        data.length = 8;
+        country = data
+        console.log(data)
+        countries = country.map(item => {
+            return (
+                `<div id=${item.numericCode} class="country--container">
+                    <img src=${item.flags.png} alt="">
+                    <div class="country--desc">
+                        <p class="country--name">${item.name}</p>
+                        <p class="population">Population: <span>${item.population}</span></p>
+                        <p class="region">Region: <span>${item.region}</span></p>
+                        <p class="capital">Capital: <span>${item.capital}</span></p>
+                    </div>
+                </div>`
+            )
+        })
+        console.log(countries)
+        $("#country--list").append(countries)
+    })
+console.log(countries)
+
+function searchByRegion(e){
+    fetch("./data.json")
+    .then(res=>res.json())
+    .then(data=>{
+        // data.length = 8;
+        country = data.filter(con=>con.region == e)
+        console.log(country)
+        countries = country.map(item => {
+            return (
+                `<div id=${item.numericCode} class="country--container">
+                    <img src=${item.flags.png} alt="">
+                    <div class="country--desc">
+                        <p class="country--name">${item.name}</p>
+                        <p class="population">Population: <span>${item.population}</span></p>
+                        <p class="region">Sub-region: <span>${item.subregion}</span></p>
+                        <p class="capital">Capital: <span>${item.capital}</span></p>
+                        <p class="capital">Demonym: <span>${item.demonym}</span></p>
+                    </div>
+                </div>`
+            )
+        })
+        console.log(countries)
+        $("#country--list").children().remove();
+        $("#country--list").append(countries)
+        $(".regions").toggle()
+    })
+console.log(countries)
+}
+
+function showMenu(){
+    $(".regions").toggle()
+}
